7f7e0de9dfe4f786fa303297ea33fb6f4763dd35
ishank011 <ishank011@gmail.com>
2016-03-03 16:57:25 +0530

    minor3

--- OpenDataAPI/settings.py
+++ OpenDataAPI/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'healthcare',
+        'NAME': 'ishank011$healthcare',
         'USER' : 'ishank011',
         'PASSWORD' : 'hell1212',
         'HOST' : '127.0.0.1',
